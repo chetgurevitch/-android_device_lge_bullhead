aec9dc00c40271e8f678f4aa5e2ec8bc51f7cf0c
Chet Gurevitch <chetgurevitch@protonmail.com>
2017-09-02 04:08:39 -0700

    Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,69 @@
+#
+# Copyright (C) 2015 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8992
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8992
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno418
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_SMP := true
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+
+BOARD_KERNEL_CMDLINE := androidboot.hardware=bullhead androidboot.console=ttyHSL0 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5
+
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
+
+# prebuilt kernel
+TARGET_PREBUILT_KERNEL := device/lge/bullhead/kernel
+# else uncomment below to build from sauce
+# TARGET_KERNEL_SOURCE := kernel/lge/bullhead
+# TARGET_KERNEL_CONFIG := bullhead_defconfig
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 41943040
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 41943040
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+
+TW_THEME := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_INCLUDE_CRYPTO := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_RECOVERY_QCOM_RTC_FIX := true
---
+++ recovery/root/sbin/pulldecryptfiles.sh
@@ -0,0 +1,43 @@
+#!/sbin/sh
+
+# This pulls the files that are needed for decrypt
+# allowing us to leave /vendor and /system unmounted
+
+if [ "$(mount|grep "/dev/block/bootdevice/by-name/system on /system ")" == "" ];then
+        mount -o rw,remount rootfs /
+	mkdir /system
+        mount -t ext4 -o ro /dev/block/bootdevice/by-name/system /system
+	mkdir /tmp/system
+	cp -r /system/lib64 /tmp/system/
+	cp -r /system/bin /tmp/system/
+	umount /system
+	mkdir /system/lib64
+	mkdir /system/bin
+	mv /tmp/system/lib64/* /system/lib64/
+	mv /tmp/system/bin/* /system/bin/
+	rmdir /tmp/system/lib64 /tmp/system/bin /tmp/system
+fi
+
+if [ "$(mount|grep "/dev/block/bootdevice/by-name/userdata on /data")" == "" ];then
+	LD_LIBRARY_PATH=/system/lib64 /system/bin/e2fsck  -y -E journal_only /dev/block/bootdevice/by-name/userdata
+        LD_LIBRARY_PATH=/system/lib64 /system/bin/tune2fs -Q ^usrquota,^grpquota,^prjquota /dev/block/bootdevice/by-name/userdata
+        mount /dev/block/bootdevice/by-name/userdata /data
+fi
+
+if [ "$(mount|grep "/dev/block/bootdevice/by-name/vendor on /vendor")" == "" ];then
+	mkdir -p /vendor
+	mkdir -p /tmp/vendor
+        mount -t ext4 -o ro /dev/block/bootdevice/by-name/vendor /vendor
+
+	cp -r /vendor/lib64 /tmp/vendor/
+	cp -r /vendor/bin /tmp/vendor/
+
+	umount /vendor
+
+	mv /tmp/vendor/* /vendor/
+	rmdir /tmp/vendor
+fi
+
+
+setprop pulldecryptfiles.finished 1
+exit 0
